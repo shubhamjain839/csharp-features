8173161f65b02e15e6e0b1f0aa8093cfde39b912
shubhamjain839 <shubhamjain839@gmail.com>
2019-09-03 11:28:46 +0530

    added-string-functions

---
+++ all-in-one/stringIm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace all_in_one
+{
+    class stringIm
+    {
+        static void Main(string[] arg)
+        {
+            
+            string a = "Hello! World";
+            string b = "Have A Nice Day";
+            object[] ar = { "hello","bye","hi"};
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            if (string.Compare(a, b) != 0)
+            {
+                Console.WriteLine("Strings are not equal! ");
+            }
+            else
+                Console.WriteLine("Strings are same !");
+            if(a.Contains("World"))
+                Console.WriteLine("World is in String a");
+            Console.WriteLine(b.IndexOf('A'));
+            b = string.Join(" ,", ar);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ all-in-one/stringmut.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_in_one
+{
+    class stringmut
+    {
+        static void Main(string[] arg)
+        {
+            StringBuilder ab = new StringBuilder(20);
+            ab.Append("hello ");
+            Console.WriteLine(ab);
+            ab.Insert(6, "World! ");
+            Console.WriteLine(ab);
+            ab.Insert(13, "bye");
+            Console.WriteLine(ab);
+            ab.Remove(12, 4);
+            Console.WriteLine(ab);
+            ab.Replace("hello ", "bye ");
+            Console.WriteLine(ab);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
