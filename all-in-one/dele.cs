3acba59bd3196d5fd711c6ce4a9520332f709d4b
shubhamjain839 <shubhamjain839@gmail.com>
2019-09-12 23:45:24 +0530

    added-delegates

---
+++ all-in-one/dele.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_in_one
+{
+    class dele
+    {
+        delegate void abc();
+        public void print_hello()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void print_bye()
+        {
+            Console.WriteLine("bye");
+        }
+        public void print_goodnight()
+        {
+            Console.WriteLine("goodnight");
+        }
+        public void print_goodmorning()
+        {
+            Console.WriteLine("goodmorning");
+        }
+
+        static void Main(string[] arg)
+        {
+            dele d1 = new dele();
+            abc a1, a2, a3, a4;
+            a1 = d1.print_hello;
+            a2 = d1.print_bye;
+            a3 = a1 + a2;
+            a4 = a3 - a2;
+            a1();
+            a3();
+            a4();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ all-in-one/inher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_in_one
+{
+    class shape
+    {
+        public int side;
+        public void get_side(int tside) {
+            side = tside;
+        }
+    }
+    class rectangle : shape
+    {
+        int wid;
+        public void get_wid(int twid) { wid = twid; }
+        public void cal_area() { Console.WriteLine("Area of Rectangle is :" + side * wid); }
+    }
+    class square : shape
+    {
+        public void cal_area() { Console.WriteLine("area of square is: " + side * side);  }
+    }
+
+    class inher
+    {
+        static void Main(string[] arg)
+        {
+            square S1 = new square();
+            rectangle R1 = new rectangle();
+
+            R1.get_side(12);
+            R1.get_wid(2);
+            R1.cal_area();
+
+            S1.get_side(10);
+            S1.cal_area();
+
+
+            Console.ReadKey();
+        }
+    }
+}
